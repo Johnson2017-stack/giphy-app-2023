65df80adcd3bd6b39c26131e1c637072b2c82559
User <user@Andys-iMac.lan>
2023-05-31 18:17:03 -0700

    giphy app

---
+++ script.js
@@ -0,0 +1,30 @@
+const gifContainerEl = document.querySelector('#gif-container');
+
+function getGifs(){
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=VY7qYdyjIMP1sHz0mFICNcZ970oims2s&limit=10&rating="g"')
+    .then((response) => response.json())
+    .then((data) => {
+
+        const results = data.data;
+
+        console.log(results);
+
+        results.map(gif => {
+            let title = document.createElement('h4')
+            let giphyBox = document.createElement('div')
+
+            title.textContent = gif.title;
+            giphyBox.innerHTML = `<img src="${gif.images.original.url}" alt="${gif.title}" />`;
+
+            console.log(giphyBox);
+
+            gifContainerEl.append(title);
+            gifContainerEl.append(giphyBox);
+
+
+            
+        })
+    })
+}
+
+getGifs();
